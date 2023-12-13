8def6f8b4099a65e4ca986f42e07a5fd6823f03f
Rachanan2608 <153697509+Rachanan2608@users.noreply.github.com>
2023-12-13 18:32:48 +0530

    Quadratic.java

--- Quadratic.java
+++ Quadratic.java
@@ -43,7 +43,7 @@ class Quadratic {
 
 class QuadraticMain {
     public static void main(String[] args) {
-        System.out.println("My Name is Rachana R");
+        System.out.println("My Name is Rachana N");
         System.out.println("My USN is 2023BMS0214");
         Quadratic quadratic = new Quadratic();
         quadratic.getCoefficients();
